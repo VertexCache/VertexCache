39d40ef1a1be43896b03a1f1068e48b67ba3f14a
Jason Lam <jasonlam604@gmail.com>
2025-06-01 08:57:46 -0700

    Minor package move, wrong spot

--- client-sdks/java/sdk/src/test/java/com/vertexcache/sdk/exception/VertexCacheSdkExceptionTest.java
+++ client-sdks/java/sdk/src/test/java/com/vertexcache/sdk/exception/VertexCacheSdkExceptionTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.vertexcache.sdk.result;
+package com.vertexcache.sdk.exception;
 
 import com.vertexcache.sdk.exception.VertexCacheSdkException;
 import org.junit.jupiter.api.Test;
