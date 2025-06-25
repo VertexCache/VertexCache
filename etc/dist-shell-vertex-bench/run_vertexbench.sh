8ec3219d1e3ef8d3d99bba536b1d0b7283089a43
Jason Lam <jasonlam604@gmail.com>
2025-06-25 15:10:57 -0700

    Inserted placeholder data as, hint for what the Public key should look like (that's probably the most complicated attribute the reset are straight-forward)

--- etc/dist-shell-vertex-bench/run_vertexbench.sh
+++ etc/dist-shell-vertex-bench/run_vertexbench.sh
@@ -8,11 +8,11 @@ java -Xms1g -Xmx2g -XX:+UseG1GC -XX:+AlwaysPreTouch -Xlog:gc*,safepoint:file=gc.
   "percentageWrites": 25,
   "totalKeyCount": 5000,
   "clientId": "sdk-client-java",
-  "clientToken": "need-valid-token-here",
+  "clientToken": "ea143c- a valid token goes here ffe4a6c7",
   "serverHost": "localhost",
   "serverPort": 50505,
   "enableTlsEncryption": true,
   "enablePreload": true,
   "encryptionMode": "ASYMMETRIC",
-  "publicKey": "Need-Valid-Key-Here"
+  "publicKey": "-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqyx+GpSo6UmkrvXknkmpS----- This is one line with \n --------+dWIakA8KZYQ95gP6oawmXiu2PcPeV\nEwIDAQAB\n-----END PUBLIC KEY-----\n"
 }'
