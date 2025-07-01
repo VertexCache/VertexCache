a72ea9d1b501631db6b7fadd75ddb9c8213a9e54
Jason Lam <jasonlam604@gmail.com>
2025-07-01 13:28:18 -0700

    Kotlin changes for publishing

--- client-sdks/kotlin/build.gradle.kts
+++ client-sdks/kotlin/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     kotlin("jvm") version "1.9.23"
+    `maven-publish`
+    signing
 }
 
 group = "com.vertexcache"
@@ -16,8 +18,6 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
-
-    // 👇 Enable printing of stdout/stderr
     testLogging {
         events("passed", "failed", "skipped", "standard_out", "standard_error")
         showStandardStreams = true
@@ -33,16 +33,64 @@ sourceSets {
     }
 }
 
-// Optional: Publishing block
-/*
+// ➤ Top-level publishing block
 publishing {
     publications {
         create<MavenPublication>("mavenJava") {
             from(components["java"])
             groupId = "com.vertexcache"
-            artifactId = "vertexcache-sdk"
-            version = "1.0.0"
+            artifactId = "vertexcache-sdk-kotlin"
+            version = project.version.toString()
+
+            pom {
+                name.set("VertexCache Kotlin SDK")
+                description.set("Kotlin client library for VertexCache")
+                url.set("https://github.com/VertexCache/VertexCache/tree/main/client-sdks/kotlin")
+
+                licenses {
+                    license {
+                        name.set("Apache License 2.0")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0.html")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git:git://github.com/VertexCache/VertexCache.git")
+                    developerConnection.set("scm:git:ssh://git@github.com:VertexCache/VertexCache.git")
+                    url.set("https://github.com/VertexCache/VertexCache")
+                }
+
+                developers {
+                    developer {
+                        id.set("jasonlam604")
+                        name.set("Jason Lam")
+                        email.set("contact@vertexcache.com")
+                    }
+                }
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            name = "OSSRH"
+            url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+
+            val usernameProp = findProperty("ossrhUsername")?.toString() ?: throw GradleException("ossrhUsername not set")
+            val passwordProp = findProperty("ossrhPassword")?.toString() ?: throw GradleException("ossrhPassword not set")
+
+            println("DEBUG - OSSRH Username Resolved: $usernameProp")
+            println("DEBUG - OSSRH Password Length: ${passwordProp.length}")
+
+            credentials {
+                username = usernameProp
+                password = passwordProp
+            }
         }
     }
 }
-*/
+
+signing {
+    useGpgCmd()
+    sign(publishing.publications["mavenJava"])
+}
