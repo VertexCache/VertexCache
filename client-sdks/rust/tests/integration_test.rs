d62397fbd2674742b15f4d9e63a17132775e8c08
Jason Lam <jasonlam604@gmail.com>
2025-07-01 17:00:06 -0700

    Rust SDK: Finalize Cargo.toml metadata and update integration test structure

--- client-sdks/rust/tests/integration_test.rs
+++ client-sdks/rust/tests/integration_test.rs
@@ -2,23 +2,26 @@
 // Copyright 2025 to Present, Jason Lam - VertexCache (https://github.com/vertexcache/vertexcache)
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
 // ------------------------------------------------------------------------------
-
-use vertexcache_sdk::ping;
+use vertexcache_sdk::comm::client_connector::ClientConnector;
+use vertexcache_sdk::command::ping_command::PingCommand;
+use vertexcache_sdk::model::client_option::ClientOption;
+use vertexcache_sdk::model::encryption_mode::EncryptionMode;
 
 #[test]
-fn test_ping_should_return_pong() {
-    let result = ping();
-    println!("Ping result: {}", result);
-    assert_eq!(result, "PONG");
+fn test_ping_command_should_succeed_with_insecure_tls() {
+    let mut option = ClientOption::new();
+    option.set_server_host("127.0.0.1".to_string());
+    option.set_server_port(50505);
+    option.set_enable_tls_encryption(true);
+    option.set_verify_certificate(false);
+    option.set_connect_timeout(2000);
+    option.set_read_timeout(2000);
+
+    let mut sdk = VertexCacheSDK::new(option);
+
+    sdk.open_connection().expect("Failed to connect to server");
+    let ping_result = sdk.ping();
+
+    assert!(ping_result.success, "Ping failed: {}", ping_result.message);
 }
